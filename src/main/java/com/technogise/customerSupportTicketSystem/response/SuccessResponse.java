a99d9a991f66dd00f6b832876e7ce1c54b1e7704
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-17 15:53:20 +0530

    feat: add generic SuccessResponse wrapper class with static factory method

---
+++ src/main/java/com/technogise/customerSupportTicketSystem/response/SuccessResponse.java
@@ -0,0 +1,41 @@
+package com.technogise.customerSupportTicketSystem.response;
+
+public class SuccessResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public SuccessResponse(boolean success ,String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public static <T> SuccessResponse<T> success(String message, T data) {
+        return new SuccessResponse<>(true, message, data);
+    }
+}
