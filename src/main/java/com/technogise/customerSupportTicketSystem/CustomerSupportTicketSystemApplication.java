e54a517851a3222b9f4acd2c743e79f5962ba85a
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-17 13:16:59 +0530

    chore: initial project set-up

---
+++ src/main/java/com/technogise/customerSupportTicketSystem/CustomerSupportTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.technogise.customerSupportTicketSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSupportTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSupportTicketSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technogise/customerSupportTicketSystem/CustomerSupportTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technogise.customerSupportTicketSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSupportTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
