455730a3b75048bfc1f4abc7c0023ad45f1f0fcc
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-17 18:09:12 +0530

    feat: create the repository for user and comment

--- src/main/java/com/technogise/customerSupportTicketSystem/repository/CommentRepository.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/repository/CommentRepository.java
@@ -1,4 +1,9 @@
 package com.technogise.customerSupportTicketSystem.repository;
 
-public interface CommentRepository {
+import com.technogise.customerSupportTicketSystem.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
 }
