91791e514e1375119fe078c82727ffe620c989a8
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-17 20:19:50 +0530

    feat: add dto for comment request and comment response

--- src/main/java/com/technogise/customerSupportTicketSystem/dto/CreateCommentRequest.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/dto/CreateCommentRequest.java
@@ -1,4 +1,14 @@
 package com.technogise.customerSupportTicketSystem.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateCommentRequest {
+    @NotBlank(message = "Comment cannot be blank or null")
+    @Size(max = 1000)
+    private String body;
 }
--- src/main/java/com/technogise/customerSupportTicketSystem/dto/CreateCommentResponse.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/dto/CreateCommentResponse.java
@@ -1,4 +1,12 @@
 package com.technogise.customerSupportTicketSystem.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class CreateCommentResponse {
+    private String body;
 }
