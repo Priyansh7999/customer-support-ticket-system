4c4d710a420bf143df12dc5d3f5b3ff0c325ea84
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-18 12:29:46 +0530

    feat: add the findByEmail method in repository
    # Conflicts:
    #	src/main/java/com/technogise/customerSupportTicketSystem/repository/UserRepository.java

--- src/main/java/com/technogise/customerSupportTicketSystem/repository/UserRepository.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.technogise.customerSupportTicketSystem.repository;
 import com.technogise.customerSupportTicketSystem.enums.UserRole;
 import com.technogise.customerSupportTicketSystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +12,6 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findFirstByRole(UserRole role);
+    Optional<User> findByIdAndRole(UUID id, UserRole role);
+    Optional<User> findByEmail(String email);
 }
