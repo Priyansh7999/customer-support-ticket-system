c6f6a5f26b7c162cbb52c0599aebedcdf445e3ac
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-18 17:50:31 +0530

    refactor: update the import of ticket files

--- src/main/java/com/technogise/customerSupportTicketSystem/controller/TicketController.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/controller/TicketController.java
@@ -1,20 +1,19 @@
 package com.technogise.customerSupportTicketSystem.controller;
 
+import com.technogise.customerSupportTicketSystem.constant.Constants;
+import com.technogise.customerSupportTicketSystem.dto.CreateTicketRequest;
+import com.technogise.customerSupportTicketSystem.dto.CreateTicketResponse;
+import org.springframework.web.bind.annotation.*;
 import com.technogise.customerSupportTicketSystem.dto.CreateCommentRequest;
 import com.technogise.customerSupportTicketSystem.dto.CreateCommentResponse;
 import com.technogise.customerSupportTicketSystem.response.SuccessResponse;
 import com.technogise.customerSupportTicketSystem.service.TicketService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
-import com.technogise.customerSupportTicketSystem.constant.Constants;
-import com.technogise.customerSupportTicketSystem.dto.CreateTicketRequest;
-import com.technogise.customerSupportTicketSystem.dto.CreateTicketResponse;
-import org.springframework.web.bind.annotation.*;
 
 
 @RestController
