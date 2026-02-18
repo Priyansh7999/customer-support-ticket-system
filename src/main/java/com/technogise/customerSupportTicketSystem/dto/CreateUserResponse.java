db858706d6d03d2b09d0d3dc80f6becbf939ddb7
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-18 12:47:57 +0530

    feat:create DTO CreateUserResponse

---
+++ src/main/java/com/technogise/customerSupportTicketSystem/dto/CreateUserResponse.java
@@ -0,0 +1,20 @@
+package com.technogise.customerSupportTicketSystem.dto;
+
+import com.technogise.customerSupportTicketSystem.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateUserResponse {
+    private String name;
+    private String email;
+    private UserRole role;
+    private UUID userId;
+}
