79b638997d278b5475532bb6d39faa9e9e0a1c06
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-18 10:07:11 +0530

    feat: add unit test for DTO's UUID attributes validations in TicketAssignmentControllerTest

--- src/test/java/com/technogise/customerSupportTicketSystem/controller/TicketAssignmentControllerTest.java
+++ src/test/java/com/technogise/customerSupportTicketSystem/controller/TicketAssignmentControllerTest.java
@@ -2,9 +2,7 @@ package com.technogise.customerSupportTicketSystem.controller;
 
 import com.technogise.customerSupportTicketSystem.dto.TicketAssignmentRequest;
 import com.technogise.customerSupportTicketSystem.dto.TicketAssignmentResponse;
-import com.technogise.customerSupportTicketSystem.model.TicketAssignment;
 import com.technogise.customerSupportTicketSystem.service.TicketAssignmentService;
-import jakarta.websocket.SendResult;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +10,11 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import tools.jackson.databind.ObjectMapper;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -71,4 +68,34 @@ class TicketAssignmentControllerTest {
                 .andExpect(jsonPath("$.message").value("Ticket Assigned Successfully"));
 
     }
+    @Test
+    void shouldThrowException_whenTheUUIDFormatIsInvalid() throws Exception {
+
+        String invalidJson = """
+        {
+          "assignedToUserId": "501a4912-4351-428b-a4ed-971e28ee1086",
+          "assignedByUserId": "invalid-uuid"
+        }
+        """;
+        UUID ticketId = UUID.randomUUID();
+
+        System.out.println(ticketId);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/tickets/{id}/assign",ticketId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidJson))
+                .andExpect(status().isBadRequest());
+
+
+        String invalidJson2 = """
+        {
+          "assignedToUserId": "invalid-uuid",
+          "assignedByUserId": "501a4912-4351-428b-a4ed-971e28ee1086"
+        }
+        """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/tickets/{id}/assign",ticketId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidJson2))
+                .andExpect(status().isBadRequest());
+    }
+
 }
