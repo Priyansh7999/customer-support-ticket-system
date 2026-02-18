9cb1498668132bcfd40fc2b67959eedfcbbf7cef
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-18 12:08:47 +0530

    refactor: change the name of handleResourceNotFound to handleResourceNotFoundException in global exception file

--- src/main/java/com/technogise/customerSupportTicketSystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/exception/GlobalExceptionHandler.java
@@ -14,7 +14,7 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getCode(), ex.getMessage()));
     }
 
