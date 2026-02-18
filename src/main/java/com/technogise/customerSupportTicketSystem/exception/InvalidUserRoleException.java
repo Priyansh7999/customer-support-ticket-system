af53d6ee68126089fff2be9d543438271903cee2
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-18 18:43:14 +0530

    fix: set code attribute as final in InvalidUserRoleException

--- src/main/java/com/technogise/customerSupportTicketSystem/exception/InvalidUserRoleException.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/exception/InvalidUserRoleException.java
@@ -4,12 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class InvalidUserRoleException extends RuntimeException {
-<<<<<<< HEAD
-    private String code;
-
-=======
     private final String code;
->>>>>>> 70208a9 (refactor: rename the AgentRoleNotFoundException to InvalidUserRoleException)
     public InvalidUserRoleException(String code, String message) {
         super(message);
         this.code = code;
