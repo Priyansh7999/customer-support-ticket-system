1eaa83f1dc16aee370df6a9010b9b69470b077d5
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-18 12:26:25 +0530

    feat(controller): add createTicket endpoint with validation and error handling tests

--- src/main/java/com/technogise/customerSupportTicketSystem/controller/TicketController.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/controller/TicketController.java
@@ -1,9 +1,44 @@
 package com.technogise.customerSupportTicketSystem.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.technogise.customerSupportTicketSystem.dto.CreateTicketRequest;
+import com.technogise.customerSupportTicketSystem.dto.CreateTicketResponse;
+import com.technogise.customerSupportTicketSystem.model.Ticket;
+import com.technogise.customerSupportTicketSystem.response.SuccessResponse;
+import com.technogise.customerSupportTicketSystem.service.TicketService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/tickets")
 public class TicketController {
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping
+    public ResponseEntity<SuccessResponse<CreateTicketResponse>> createTicket(
+            @Valid @RequestBody CreateTicketRequest request,
+            @RequestHeader ("User-Id")UUID userId) {
+
+        String title = request.getTitle();
+        String description = request.getDescription();
+
+        Ticket createdTicket = ticketService.createTicket(title, description, userId);
+
+        CreateTicketResponse response = new CreateTicketResponse();
+        response.setId(createdTicket.getId());
+        response.setTitle(createdTicket.getTitle());
+        response.setDescription(createdTicket.getDescription());
+        response.setStatus(createdTicket.getStatus());
+        response.setAssignedToName(createdTicket.getAssignedTo().getName());
+        response.setCreatedAt(createdTicket.getCreatedAt());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.success("Ticket created successfully", response));
+    }
 }
