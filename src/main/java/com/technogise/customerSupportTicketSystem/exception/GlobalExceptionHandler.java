ecf51a2d5a8ef6482330a71c43ca171f819c0eed
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-18 18:09:44 +0530

    fix: remove the MethodArgumentNotValidException in global exception file

--- src/main/java/com/technogise/customerSupportTicketSystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/exception/GlobalExceptionHandler.java
@@ -57,6 +57,12 @@ public class GlobalExceptionHandler {
         ErrorResponse error = new ErrorResponse("INVALID_REQUEST",fieldError.getDefaultMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex) {
+//        FieldError fieldError = ex.getBindingResult().getFieldErrors().getFirst();
+//        ErrorResponse error = new ErrorResponse("INVALID_REQUEST",fieldError.getDefaultMessage());
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+//    }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorResponse> handleAccessDenied(AccessDeniedException ex) {
