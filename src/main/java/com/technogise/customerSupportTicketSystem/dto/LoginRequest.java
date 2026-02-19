a015968719bdd1eec37d6df0d869a5c4d4afde72
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-19 18:44:47 +0530

    feat: add login request dto

--- src/main/java/com/technogise/customerSupportTicketSystem/dto/LoginRequest.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/dto/LoginRequest.java
@@ -2,15 +2,17 @@ package com.technogise.customerSupportTicketSystem.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
 
-    @Email
-    @NotBlank
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email is required")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 }
