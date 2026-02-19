87d430d8f507708109a139f05165842c75d60e22
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-19 18:32:55 +0530

    feat: update the user entity to add UserDetail security
    # Conflicts:
    #	src/main/java/com/technogise/customerSupportTicketSystem/model/User.java
    #	src/main/java/com/technogise/customerSupportTicketSystem/repository/UserRepository.java

---
+++ src/main/java/com/technogise/customerSupportTicketSystem/dto/RegisterRequest.java
@@ -0,0 +1,9 @@
+package com.technogise.customerSupportTicketSystem.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+
+}
