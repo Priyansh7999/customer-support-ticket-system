33fb6a3c2052b2c925645fade6e5871a45840d9f
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-20 13:17:25 +0530

    feat: add password attribute in the user entity

--- src/main/java/com/technogise/customerSupportTicketSystem/model/User.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/model/User.java
@@ -26,6 +26,9 @@ public class User {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    @Column(name = "password", nullable = false)
+    private String password;
+
     @Column(name = "role", nullable = false)
     @Enumerated(EnumType.STRING)
     private UserRole role;
