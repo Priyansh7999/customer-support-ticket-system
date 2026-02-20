864f4f4ea23bf732b70320247f30f7994a838d09
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-20 19:37:01 +0530

    feat: create Login response dto

--- src/main/java/com/technogise/customerSupportTicketSystem/dto/LoginResponse.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/dto/LoginResponse.java
@@ -1,4 +1,13 @@
 package com.technogise.customerSupportTicketSystem.dto;
 
+import lombok.*;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
 public class LoginResponse {
+    private String token;
+    private UUID userId;
+    private String email;
+    private String role;
 }
