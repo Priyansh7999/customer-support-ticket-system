7529368fd9010288d53420eaf10b412fedd73651
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-20 15:24:12 +0530

    feat(security): implement JwtService for JWT generation and claim extraction

--- src/main/java/com/technogise/customerSupportTicketSystem/service/JwtService.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.technogise.customerSupportTicketSystem.service;
 
+import com.technogise.customerSupportTicketSystem.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.UUID;
 import java.util.function.Function;
 
 @Service
@@ -17,43 +19,52 @@ public class JwtService {
 
     @Value("${application.security.jwt.secret-key}")
     private String secretKey;
-
     @Value("${application.security.jwt.expiration}")
     private long expiration;
 
-    //extract email from token
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
-    //generate token for user
-    public String generateToken(UserDetails userDetails) {
+
+    public String generateToken(User  user) {
         return Jwts.builder()
-                .subject(userDetails.getUsername())
+                .subject(user.getEmail())
+                .claim("userId", user.getId())
+                .claim("role", user.getRole())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis()+expiration))
                 .signWith(getSigningKey())
                 .compact();
     }
-    //Validate token
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        return extractUsername(token).equals(userDetails.getUsername())
-                && !isTokenExpired(token);
+
+    //extract email from token
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
     }
 
-    private boolean isTokenExpired(String token) {
-        return extractClaim(token, Claims::getExpiration).before(new Date());
-    }
-    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
-        return resolver.apply(
-                Jwts.parser()
-                        .verifyWith(getSigningKey())
-                        .build()
-                        .parseSignedClaims(token)
-                        .getPayload()
-        );
+    //extract user-id from token
+    public String extractUserId(String token){
+        String userId = extractAllClaims(token).get("userId",String.class);
+        return UUID.fromString(userId).toString();
     }
 
-    private SecretKey getSigningKey() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
+
+
+
+//    //Validate token
+//    public boolean isTokenValid(String token, UserDetails userDetails) {
+//        return extractUsername(token).equals(userDetails.getUsername())
+//                && !isTokenExpired(token);
+//    }
+//
+//    private boolean isTokenExpired(String token) {
+//        return extractClaim(token, Claims::getExpiration).before(new Date());
+//    }
 }
