9e8752352548d4e3012f0e101b368f20681fd462
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-20 14:50:18 +0530

    refactor: replace constructor with lombok annotations in agent ticket response dto

--- src/main/java/com/technogise/customerSupportTicketSystem/dto/AgentTicketResponse.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/dto/AgentTicketResponse.java
@@ -2,23 +2,19 @@ package com.technogise.customerSupportTicketSystem.dto;
 
 import com.technogise.customerSupportTicketSystem.enums.TicketPriority;
 import com.technogise.customerSupportTicketSystem.enums.TicketStatus;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AgentTicketResponse implements TicketView {
     String title;
     String description;
     TicketStatus status;
     TicketPriority priority;
     LocalDateTime createdAt;
-
-    public AgentTicketResponse(String title, String description, TicketStatus ticketStatus, TicketPriority ticketPriority, LocalDateTime createdAt) {
-        this.title = title;
-        this.description = description;
-        this.status = ticketStatus;
-        this.priority = ticketPriority;
-        this.createdAt = createdAt;
-    }
 }
