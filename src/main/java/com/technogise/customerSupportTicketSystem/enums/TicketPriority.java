41c17aeab25c3960e952ffe559321281ea0dfaf7
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-02-21 13:25:32 +0530

    feat: add exception handling invalid enums

--- src/main/java/com/technogise/customerSupportTicketSystem/enums/TicketPriority.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/enums/TicketPriority.java
@@ -1,7 +1,20 @@
 package com.technogise.customerSupportTicketSystem.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.technogise.customerSupportTicketSystem.exception.BadRequestException;
+
 public enum TicketPriority {
     LOW,
     MEDIUM,
-    HIGH
+    HIGH;
+
+    @JsonCreator
+    public static TicketPriority from(String value) {
+        try {
+            return TicketPriority.valueOf(value);
+        } catch (IllegalArgumentException e) {
+            throw new BadRequestException("INVALID_ENUM_VALUE", "Invalid priority value: " + value);
+        }
+    }
+
 }
