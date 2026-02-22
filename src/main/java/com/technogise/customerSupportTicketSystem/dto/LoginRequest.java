0a1e399365b98e95c07f31205e1549700a0739ec
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-22 17:26:30 +0530

    fix: remove the request login dto

--- src/main/java/com/technogise/customerSupportTicketSystem/dto/LoginRequest.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/dto/LoginRequest.java
@@ -13,6 +13,5 @@ public class LoginRequest {
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 }
