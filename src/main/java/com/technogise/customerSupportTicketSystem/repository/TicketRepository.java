e5013555e5ca1e912b7a82a3d736a2ece5e6e672
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-03-01 13:16:40 +0530

    feat: add query methods to fetch tickets by customer and agent

--- src/main/java/com/technogise/customerSupportTicketSystem/repository/TicketRepository.java
+++ src/main/java/com/technogise/customerSupportTicketSystem/repository/TicketRepository.java
@@ -4,8 +4,11 @@ import com.technogise.customerSupportTicketSystem.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+    List<Ticket> findAllByCreatedById(UUID userId);
+    List<Ticket> findAllByAssignedToId(UUID userId);
 }
